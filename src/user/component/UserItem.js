181fc220fd123e9a444c7fbf94d543e2db144cc4
Prowebcoder <rahul@prowebcoder.com>
2024-01-17 16:17:38 +0530

    Second Commit

--- src/user/component/UserItem.js
+++ src/user/component/UserItem.js
@@ -1,15 +1,15 @@
 import React from "react";
-
+import "./UserItem.css";
 function UserItem(props) {
   return (
-    <li>
-      <div className="user-item">
-        <div className="user-item-content">
-          <div className="user-item-image">
-            <img src="{props.image}" alt="{props.name}" />
-          </div>
-          <div className="user-item-name">{props.name}</div>
-          <div className="user-item-placecount">{props.placecount}</div>
+    <li className="user-item">
+      <div className="user-item-content">
+        <div className="user-item-image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <div className="user-item-name">Name: {props.name}</div>
+          <div className="user-item-placecount">Place: {props.placecount}</div>
         </div>
       </div>
     </li>
--- src/user/component/UserList.js
+++ src/user/component/UserList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Users from "./Users";
 import UserItem from "./UserItem";
+import "./UserList.css";
 function UserList(props) {
   if (props.items.length === 0) {
     return <div>No Users Were Found</div>;
   }
   return (
-    <ul>
+    <ul className="list-users">
       {props.items.map((user) => {
         return (
           <UserItem
