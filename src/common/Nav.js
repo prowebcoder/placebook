8f71b8da1ea410df3d289faa3940bdd05e779c99
Prowebcoder <rahul@prowebcoder.com>
2024-01-19 12:45:16 +0530

    second day Commit

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import Avatar from "./Avatar";
 function Nav() {
+  const image =
+    "https://prowebcoder.com/cdn/shop/files/logo.png?v=1619583060&width=500";
   return (
-    <div className="navbar">
-      <Link to="/users">All Users</Link>
-      <Link to="/places">Places</Link>
-      <Link to="/:user/places">User Places</Link>
-    </div>
+    <header className="header">
+      <div className="logo">
+        <Avatar image={image} className="logo"></Avatar>
+      </div>
+      <div className="navbar">
+        <Link to="/users">All Users</Link>
+        <Link to="/places">Places</Link>
+        <Link to="/:user/places">User Places</Link>
+      </div>
+    </header>
   );
 }
 
--- src/user/component/UserItem.js
+++ src/user/component/UserItem.js
@@ -6,22 +6,20 @@ import Card from "./Card";
 function UserItem(props) {
   return (
     <li className="user-item">
-      
-        <Card  className="user-item-content">
-          <Link to={`/${props.id}/places`} >
-            <div className="user-item-image">
-              <Avatar image={props.image} alt={props.name} />
+      <Card className="user-item-content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item-image">
+            <Avatar image={props.image} alt={props.name} className="xyz" />
+          </div>
+          <div className="user-item__info">
+            <div className="user-item-name">Name: {props.name}</div>
+            <div className="user-item-placecount">
+              {props.placecount === "1" ? "Place" : "Places"}:{" "}
+              {props.placecount}
             </div>
-            <div className="user-item__info">
-              <div className="user-item-name">Name: {props.name}</div>
-              <div className="user-item-placecount">
-                {props.placecount === "1" ? "Place" : "Places"}:{" "}
-                {props.placecount}
-              </div>
-            </div>
-          </Link>
-        </Card>
-     
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 }
